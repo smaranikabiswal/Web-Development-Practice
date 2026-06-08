392f3b3a63974f3476534dfb9570f49121ad5282
smaranika biswal <23btcse80@suiit.ac.in>
2026-06-08 18:38:58 +0530

    create a simple ludo board

---
+++ React/react-state/color-board/src/LudoBoard.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+export default function LudoBoard(){
+    let [moves,setMoves]=useState({blue:0,red:0,yellow:0,green:0});
+    return (
+<div>
+   <h3>game begins!</h3> 
+   <div className="board">
+<p>Blue moves={moves.blue}</p>
+<button style={{backgroundColor:"blue"}}>+1</button>
+<p>Yellow moves={moves.yellow}</p>
+<button style={{backgroundColor:"yellow" ,color:"black"}}>+1</button>
+<p>Green moves={moves.green}</p>
+<button style={{backgroundColor:"green"}}>+1</button>
+<p>Red moves={moves.red}</p>
+<button style={{backgroundColor:"red"}}>+1</button>
+   </div>
+</div>
+    );
+}
