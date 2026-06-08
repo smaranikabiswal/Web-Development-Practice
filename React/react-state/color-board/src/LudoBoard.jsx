a831936c451c65af71c3e0b91e21b697ed2d3c60
smaranika biswal <23btcse80@suiit.ac.in>
2026-06-08 18:51:06 +0530

    add functionality

--- React/react-state/color-board/src/LudoBoard.jsx
+++ React/react-state/color-board/src/LudoBoard.jsx
@@ -1,18 +1,33 @@
 import { useState } from "react";
 export default function LudoBoard(){
-    let [moves,setMoves]=useState({blue:0,red:0,yellow:0,green:0});
+    let [moves,setmoves]=useState({blue:0,red:0,yellow:0,green:0});
+
+let updateBlue=()=>{
+    setmoves({...moves,blue:moves.blue+1})
+}
+let updateGreen=()=>{
+    setmoves({...moves,green:moves.green+1})
+}
+let updateYellow=()=>{
+    setmoves({...moves,yellow:moves.yellow+1})
+}
+let updateRed=()=>{
+    setmoves({...moves,red:moves.red+1})
+}
+
+
     return (
 <div>
    <h3>game begins!</h3> 
    <div className="board">
 <p>Blue moves={moves.blue}</p>
-<button style={{backgroundColor:"blue"}}>+1</button>
+<button style={{backgroundColor:"blue"}} onClick={updateBlue}>+1</button>
 <p>Yellow moves={moves.yellow}</p>
-<button style={{backgroundColor:"yellow" ,color:"black"}}>+1</button>
+<button style={{backgroundColor:"yellow" ,color:"black"}} onClick={updateYellow}>+1</button>
 <p>Green moves={moves.green}</p>
-<button style={{backgroundColor:"green"}}>+1</button>
+<button style={{backgroundColor:"green"}} onClick={updateGreen}>+1</button>
 <p>Red moves={moves.red}</p>
-<button style={{backgroundColor:"red"}}>+1</button>
+<button style={{backgroundColor:"red"}} onClick={updateRed}>+1</button>
    </div>
 </div>
     );
